3fed25433c093522d6ca1d1cbea0dc27beb0d7ad
NotAppIicable <cosmicaura3@gmail.com>
2025-01-19 20:43:13 -0600

    fix: update AprilTag field layout for 2025
    - Change APRIL_TAG_FIELD_LAYOUT to use the 2025 Reefscape configuration.
    - Replace deprecated method with loadField for loading the new layout.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,8 +28,7 @@ public final class Constants {
 
         // Pipeline settings
         public static final int APRIL_TAG_PIPELINE = 0;
-        public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFields.k2024Crescendo
-                .loadAprilTagLayoutField();
+        public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
     }
 
     public static class RaspberryPIConstants {
