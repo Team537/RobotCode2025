fa27ca389ae7cfac129a1af4ae005acec3d6a647
Patrick Wang <86264473+iapcmeimpcij2pjncr@users.noreply.github.com>
2025-02-15 11:02:32 -0600

    feat: add xBoxControlls

--- src/main/java/frc/robot/subsystems/narwhal/NarwhalClimber.java
+++ src/main/java/frc/robot/subsystems/narwhal/NarwhalClimber.java
@@ -16,6 +16,7 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.NarwhalConstants.NarwhalClimberConstants;
@@ -85,6 +86,15 @@ public class NarwhalClimber extends SubsystemBase {
     public void climb() {
         setCurrentMotorAngle(NarwhalClimberConstants.CLIMB_ANGLE);
         currentState = NarwhalClimberState.CLIMBING; // must be after the set function because the set function will default to CUSTOM state
+    }
+
+    public void runXBoxController(XboxController xBoxController){
+        if(xBoxController.getPOV() == 0){
+            goToDeploy();
+        }
+        if(xBoxController.getPOV() == 90){
+            climb();
+        }
     }
     
     @Override
