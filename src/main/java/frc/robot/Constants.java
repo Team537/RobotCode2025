d99b9679499248df772a2394ee65124e99848194
NotAppIicable <cosmicaura3@gmail.com>
2025-02-22 20:44:54 -0600

    style: reformat comments for better readability
    - Reformat long comment lines to improve readability.
    - Adjust indentation for consistency in the Constants class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,10 +21,8 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.
- * This class should not be used for any other purpose. All constants should be
- * declared globally (i.e. public static).
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants.
+ * This class should not be used for any other purpose. All constants should be declared globally (i.e. public static).
  * Do not put anything functional in this class.
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
@@ -34,12 +32,9 @@ public final class Constants {
 
     /**
      * <h2>OperatorConstants</h2>
-     * The {@code OperatorConstants} class is a subclass contained within the
-     * {@code Constants} class.
-     * This subclass contains all of the constants relating to how the robot is
-     * manually controlled.
-     * This includes things like the maximum boost mode speed, the driver controller
-     * port, driver rotational offsets, etc.
+     * The {@code OperatorConstants} class is a subclass contained within the {@code Constants} class.
+     * This subclass contains all of the constants relating to how the robot is manually controlled.
+     * This includes things like the maximum boost mode speed, the driver controller port, driver rotational offsets, etc.
      */
     public static class OperatorConstants {
         public static final int DRIVER_CONTROLLER_PORT = 0;
@@ -290,7 +285,7 @@ public final class Constants {
         }
     }
 
-        /**
+    /**
      * <h2>SquidConstants</h2>
      * The {@code SquidConstants} class is a subclass contained within the {@code Constants} class.
      * This subclass contains all of the constants relating to the Squids's mechanisms.
