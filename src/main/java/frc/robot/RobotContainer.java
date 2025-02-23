f32912e1cd60f277866cb4cbb7a19bba4a10672e
Graham Mueller <graham.mueller315@gmail.com>
2025-02-23 13:02:52 -0600

    check in a base elastic layout. create an enum-prettifier to simplify adding choosers. on enable, take upper assembly from dashboard rather than defaulting at init. remove empty test file.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import frc.robot.subsystems.vision.OceanViewManager;
 import frc.robot.subsystems.vision.odometry.PhotonVisionCamera;
 import frc.robot.subsystems.vision.odometry.VisionOdometry;
 import frc.robot.subsystems.upper_assembly.UpperAssemblyBase;
+import frc.robot.util.EnumPrettifier;
 import frc.robot.util.autonomous.Alliance;
 import frc.robot.util.autonomous.AutonomousRoutine;
 import frc.robot.util.swerve.DrivingMotor;
@@ -48,7 +49,7 @@ public class RobotContainer {
     // Subsystems
     private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
     private DriveSubsystem driveSubsystem = new DriveSubsystem();
-    private UpperAssemblyBase upperAssembly = UpperAssemblyFactory.createUpperAssembly(Constants.UpperAssemblyConstants.DEFAULT_UPPER_ASSEMBLY);
+    private UpperAssemblyBase upperAssembly;
     
     private VisionOdometry visionOdometry = new VisionOdometry(driveSubsystem.getSwerveDrivePoseEstimator()); // TODO: Add logic to add cameras to adjust odometry. visionOdometry.addCamera(PhotonVisionCamera camera);
     
@@ -133,30 +134,11 @@ public class RobotContainer {
      * This method sets up the dashboard so that the drivers can configure the robots settings.
      */
     private void setupSmartDashboard() {
-            
-        // Setup Autonomous Routine Selection
-        autonomousSelector.setDefaultOption("LEFT_HIGH_SCORE", AutonomousRoutine.LEFT_HIGH_SCORE);
-        for (AutonomousRoutine autonomousRoutine : AutonomousRoutine.values()) {
-            autonomousSelector.addOption(autonomousRoutine.toString(), autonomousRoutine);
-        }
-
-        // Setup Alliance Selection
-        allianceSelector.setDefaultOption("RED", Alliance.RED);
-        for (Alliance alliance : Alliance.values()) {
-            allianceSelector.addOption(alliance.toString(), alliance);
-        }
-
-        // Setup Upper Substructure Selection
-        upperSubstructureSelector.setDefaultOption("NARWAL", UpperAssemblyType.NARWHAL);
-        for (UpperAssemblyType upperSubstructure : UpperAssemblyType.values()) {
-            upperSubstructureSelector.addOption(upperSubstructure.toString(), upperSubstructure);
-        }
-
-        // Setup Driving Motor Selection
-        drivingMotorSelector.setDefaultOption("NEO", DrivingMotor.NEO);
-        for (DrivingMotor drivingMotor : DrivingMotor.values()) {
-            drivingMotorSelector.addOption(drivingMotor.toString(), drivingMotor);
-        }
+        // Setup dropdowns from enumeration values
+        EnumPrettifier.setupSendableChooserFromEnum(this.autonomousSelector, AutonomousRoutine.class, AutonomousRoutine.LEFT_HIGH_SCORE);
+        EnumPrettifier.setupSendableChooserFromEnum(this.allianceSelector, Alliance.class, Alliance.RED);
+        EnumPrettifier.setupSendableChooserFromEnum(this.upperSubstructureSelector, UpperAssemblyType.class, UpperAssemblyType.NARWHAL);
+        EnumPrettifier.setupSendableChooserFromEnum(this.drivingMotorSelector, DrivingMotor.class, DrivingMotor.NEO);
 
         // Add the selectors to the dashboard.
         SmartDashboard.putData(autonomousSelector);
@@ -175,6 +157,8 @@ public class RobotContainer {
         Alliance selectedAlliance = allianceSelector.getSelected();
         SmartDashboard.putString("Selected Autonomous", selectedAutonomousRoutine.toString());
         SmartDashboard.putString("Selected Alliance", selectedAlliance.toString());
+
+        this.upperAssembly = UpperAssemblyFactory.createUpperAssembly(this.upperSubstructureSelector.getSelected());
 
         // An example command will be run in autonomous
         return Autos.exampleAuto(exampleSubsystem);
