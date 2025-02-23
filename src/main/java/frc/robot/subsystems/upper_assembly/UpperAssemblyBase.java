7bcfaa6e97caa061da48d905fa91ae07c4dd8921
NotAppIicable <cosmicaura3@gmail.com>
2025-02-22 22:47:36 -0600

    docs(upper_assembly): add class documentation for UpperAssemblyBase
    Added Javadoc comments to `UpperAssemblyBase` to clarify its role as an intermediary class between the `UpperAssembly` interface and the `SubsystemBase`. This documentation includes author information and versioning details.

--- src/main/java/frc/robot/subsystems/upper_assembly/UpperAssemblyBase.java
+++ src/main/java/frc/robot/subsystems/upper_assembly/UpperAssemblyBase.java
@@ -2,10 +2,20 @@ package frc.robot.subsystems.upper_assembly;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * <h2> UpperAssemblyBase </h2>
+ * An abstract class used to serve as an intermediary class between the {@link UpperAssembly} interface and the {@link SubsystemBase}
+ * abstract class. This class allows us to use SubsystemBase functionality on the upper assemblies without having the {@link SubsystemBase}
+ * interface extend the {@link SubsystemBase} class, preventing unexpected behavior due to an interface having pre-defined, functional code
+ * contained within methods.
+ * <hr>
+ * @author Cameron Myhre
+ * @since 2.0.0
+ */
 public abstract class UpperAssemblyBase extends SubsystemBase implements UpperAssembly {
     
     @Override
-    public void disable() {
+    public void disable() { // Basic implementation of the disable method. Ensures both teams have this defined.
         System.err.println("Error: Upper Assembly does not provide functionality to disable hardware in the event of an emergency.");
     }
 }
