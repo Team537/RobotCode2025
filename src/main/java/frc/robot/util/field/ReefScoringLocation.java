2872085f4bbea0d9a5d3fab6f67345b14f940e11
NotAppIicable <cosmicaura3@gmail.com>
2025-03-02 20:12:16 -0600

    style: update link format in ReefScoringLocation comments
    Improved the formatting of the reference link in the Javadoc for better readability.

--- src/main/java/frc/robot/util/field/ReefScoringLocation.java
+++ src/main/java/frc/robot/util/field/ReefScoringLocation.java
@@ -4,7 +4,8 @@ package frc.robot.util.field;
  * <h2> ReefScoringLocation </h2>
  * An enum value used to represent the different scoring locations along the reef. These values correspond and follow
  * FIRST`s own diagrams and standards.
- * @see <a href=https://firstfrc.blob.core.windows.net/frc2025/Manual/Sections/2025GameManual-05ARENA.pdf> FRC Reef Layout, PDF Page 6 </a>
+ * <hr>
+ * @see <a href=https://firstfrc.blob.core.windows.net/frc2025/Manual/Sections/2025GameManual-05ARENA.pdf> FRC Reef Layout </a> (PDF Page 6)
  */
 public enum ReefScoringLocation {
     A,
