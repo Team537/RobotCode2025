2004e8d82db04ce9edbb56136f270a8699193b57
NotAppIicable <cosmicaura3@gmail.com>
2025-03-02 20:10:26 -0600

    feat(field): add ReefScoringLocation enum
    Introduce ReefScoringLocation enum to represent scoring locations along the reef, aligning with FIRST's diagrams and standards.

---
+++ src/main/java/frc/robot/util/field/ReefScoringLocation.java
@@ -0,0 +1,22 @@
+package frc.robot.util.field;
+
+/**
+ * <h2> ReefScoringLocation </h2>
+ * An enum value used to represent the different scoring locations along the reef. These values correspond and follow
+ * FIRST`s own diagrams and standards.
+ * @see <a href=https://firstfrc.blob.core.windows.net/frc2025/Manual/Sections/2025GameManual-05ARENA.pdf> FRC Reef Layout, PDF Page 6 </a>
+ */
+public enum ReefScoringLocation {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L;
+}
