e8f2a504268d84a91f5686df3a16d807b8f48ff0
Graham Mueller <graham.mueller315@gmail.com>
2025-03-12 10:30:30 -0500

    sigh. you have to actually save files before committing.

--- src/main/java/frc/robot/routine/IRoutine.java
+++ src/main/java/frc/robot/routine/IRoutine.java
@@ -3,5 +3,5 @@ package frc.robot.routine;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface IRoutine {
-    Command getCommandSet()
+    Command getCommandSet();
 }
---
+++ src/main/java/frc/robot/routine/SimpleAutomatedScoringRoutine.java
@@ -0,0 +1,38 @@
+package frc.robot.routine;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.upper_assembly.UpperAssembly;
+import frc.robot.util.autonomous.Alliance;
+import frc.robot.util.autonomous.StartingPosition;
+import frc.robot.util.upper_assembly.ScoringHeight;
+
+public class SimpleAutomatedScoringRoutine implements IRoutine {
+    private final Alliance alliance;
+    private final DriveSubsystem driveSubsystem;
+    private final StartingPosition startingPosition;
+    private final UpperAssembly upperAssembly;
+
+    public SimpleAutomatedScoringRoutine(Alliance alliance, StartingPosition startingPosition, DriveSubsystem driveSubsystem, UpperAssembly upperAssembly) {
+        this.alliance = alliance;
+        this.driveSubsystem = driveSubsystem;
+        this.startingPosition = startingPosition;
+        this.upperAssembly = upperAssembly;
+    }
+
+    public Command getCommandSet() {
+        Pose2d startingPoint = new Pose2d(); // todo this should be based on our alliance and starting position (left center right)
+        Pose2d initalScoringPosition = new Pose2d(new Translation2d(11.845, 4.179), new Rotation2d(Math.PI)); // TODO determine this from starting position / etc
+        Pose2d driversStationPosition = new Pose2d(); // TODO this will also vary based on where our human player is
+
+
+        return new InstantCommand(() -> this.driveSubsystem.getPathfindingCommand(initalScoringPosition))
+            .andThen(() -> this.upperAssembly.getCoralScoreCommand(ScoringHeight.L4))
+            .andThen(() -> this.driveSubsystem.getPathfindingCommand(driversStationPosition))
+            .andThen(() -> this.upperAssembly.getCoralIntakeCommand());
+    }
+}
