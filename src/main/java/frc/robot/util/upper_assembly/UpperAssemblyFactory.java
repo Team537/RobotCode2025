c9503ea23c02fb717c9c4137220329afb7e4062d
NotAppIicable <cosmicaura3@gmail.com>
2025-03-19 10:30:35 -0500

    docs: correct comments and add documentation details
    - Fix typos in `NarwhalWristState` comments.
    - Add author and version annotations to `Complex`, `DeltaTime`, and `Matrix`.
    - Introduce class-level Javadoc for `UpperAssemblyFactory`.

--- src/main/java/frc/robot/util/upper_assembly/UpperAssemblyFactory.java
+++ src/main/java/frc/robot/util/upper_assembly/UpperAssemblyFactory.java
@@ -4,6 +4,10 @@ import frc.robot.subsystems.narwhal.NarwhalUpperAssembly;
 import frc.robot.subsystems.squid.SquidUpperAssembly;
 import frc.robot.subsystems.upper_assembly.UpperAssemblyBase;
 
+/**
+ * A factory class tha constructs the upper assembly. This is used to fuel our hot-swappable code capable of 
+ * supporting two different upper assemblies.
+ */
 public class UpperAssemblyFactory {
     public static UpperAssemblyBase createUpperAssembly(UpperAssemblyType upperAssemblyType) { 
         switch (upperAssemblyType) {
