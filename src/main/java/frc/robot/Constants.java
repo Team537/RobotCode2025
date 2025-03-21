5c190360b8c559cea7756b8ce5fb317ee934a704
Patrick Wang <86264473+iapcmeimpcij2pjncr@users.noreply.github.com>
2025-03-21 13:36:14 -0500

    fix: drop L3 elevator height by 0.03

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -400,7 +400,7 @@ public final class Constants {
             public static final double MAX_HEIGHT_METERS = 2.1336; // stops the robot from ending itself
             public static final double L1_ELEVATOR_HEIGHT = 0.05; // Meters
             public static final double L2_ELEVATOR_HEIGHT = 0.07; // Meters
-            public static double L3_ELEVATOR_HEIGHT = 0.78; // Meters
+            public static double L3_ELEVATOR_HEIGHT = 0.75; // Meters
             public static final double L4_ELEVATOR_HEIGHT = 1.7; // Meters
             public static double INTAKE_ELEVATOR_HEIGHT_METERS = 0.08; // Meters
             public static final boolean MOTOR_INVERTED = true;
