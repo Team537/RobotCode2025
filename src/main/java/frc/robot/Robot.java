7d5f38f4c9bca04492ba50b96219899ffcdae2e0
Patrick Wang <86264473+iapcmeimpcij2pjncr@users.noreply.github.com>
2025-03-29 11:01:37 -0500

    feat: make it so wrist values can be updated live instead of after a reset

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -108,7 +108,7 @@ public class Robot extends TimedRobot {
     /** This function is called periodically during operator control. */
     @Override
     public void teleopPeriodic() {
-        
+        robotContainer.setWristValuesFromSmartDashbaord();
     }
 
     @Override
